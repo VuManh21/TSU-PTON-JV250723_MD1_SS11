4a52c0ab46a72d746711aac17cfeaeecd9b748bd
VuManh21 <87159958+VuManh21@users.noreply.github.com>
2025-08-19 23:58:30 +0900

    TSU-PTON-JV250723_MD1_SS11_BAI03

---
+++ MD1_SS11_bai3/main.js
@@ -0,0 +1,51 @@
+let input = prompt("hay nhap so ma ban thich ");
+if (input === null || input.trim() === "") {
+    console.log("day khong phai la so");
+    alert("day khong phai la so");
+
+} else {
+    let i = Number(prompt("hay nhap so ma ban thich "));
+    switch (i) {
+        case 1:
+            console.log("so 1");
+            alert("so 1");
+            break;
+        case 2:
+            console.log("so 2");
+            alert("so 2");
+            break;
+        case 3:
+            console.log("so 3");
+            alert("so 3");
+            break;
+        case 4:
+            console.log("so 4");
+            alert("so 4");
+            break;
+        case 5:
+            console.log("so 5");
+            alert("so 5");
+            break;
+        case 6:
+            console.log("so 6");
+            alert("so 6");
+            break;
+        case 7:
+            console.log("so 7");
+            alert("so 7");
+            break;
+        case 8:
+            console.log("so 8");
+            alert("so 8");
+            break;
+        case 9:
+            console.log("so 9");
+            alert("so 9");
+            break;
+        default:
+            console.log("gia tri qua lon ");
+            alert("gia tri qua lon");
+
+
+    }
+}
